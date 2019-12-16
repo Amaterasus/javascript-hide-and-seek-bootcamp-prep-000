4aced4c677af66a94682f0e07e75b2a47d3acc2a
Skye Geraghty <skyetay03@googlemail.com>
2019-12-16 09:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selecting)
+{
+  return document.querySelector(selecting);
+}
 
+function nestedTarget()
+{
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n)
+{
+  const list = document.querySelector("#app").querySelectorAll('ul.ranked-list li');
+  
+  for (var i = 0; i < list.length; i++)
+  {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild()
+{
+  const nested = document.querySelector("#grand-node");
+  
+  while(nested)
+  {
+    
+  }
+  
+  return nested.innerText;
+  
+}
